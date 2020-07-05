2fd22aadd5c85614dfb27abc9e9fb4b58884fa61
kashifrazabb <kashifrazabb@gmail.com>
2020-07-05 11:18:30 -0700

    select

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -5,7 +5,7 @@ const Select = ({handleSelect,selectValue,countries}) => {
     return (
         
         <div className='container select-container '>
-            <select onChange={handleSelect} value={selectValue} className='float-right shadow'>
+            <select onChange={handleSelect} value={selectValue} className='float-right form-control shadow'>
                 <option value="" disbaled selected>Select Country</option>
                 {countries.map(i=><option value={i}>{i}</option>)}
             </select>
