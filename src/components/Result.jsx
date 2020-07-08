970e0f07a85a6b91561774f5ccf193d2b1493cd3
kashifrazabb <kashifrazabb@gmail.com>
2020-07-08 07:44:01 -0700

    ...

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ResultCard from './ResultCard';
 
 const Result = ({selectValue,date,country:{ Confirmed,Recovered,Deaths},global:{TotalConfirmed,TotalRecovered,TotalDeaths}}) => {
+
     return (
         <div className='container mt-5'>
             <div className="row justify-content-between">
-                    <ResultCard color='red' type='Infected' number={selectValue==='Global'?TotalConfirmed:Confirmed} date={date} about='Number of Infected Cases by ' />
+                    <ResultCard color='red' type='Confirmed' number={selectValue==='Global'?TotalConfirmed:Confirmed} date={date} about='Number of Infected Cases by ' />
                     <ResultCard color='blue' type='Recovered' number={selectValue==='Global'?TotalRecovered:Recovered} date={date} about='Number of Recovered Cases from '/>
                     <ResultCard color='green' type='Death' number={selectValue==='Global'?TotalDeaths:Deaths} date={date} about='Number of Deaths caused by '/>
             </div>
--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CountUp from 'react-countup';
 
 const ResultCard = ({color,type,about,number,date}) => {
 
@@ -6,10 +7,10 @@ const ResultCard = ({color,type,about,number,date}) => {
         
         <div className={`col-md-3 position-relative mb-3 result-card result-card-color-${color}`}>
             <h3>{type}</h3>
-            <h1>{parseInt(number).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</h1>
+            <h1>{number===undefined?'Loading...':<CountUp end={number} separator=','/>}</h1>
             <div className='fixed-bottom position-absolute mx-auto w-75 text-center'>
-                <p><b>Updated:</b> {new Date(date).toDateString()}</p>
-                <p>{about}<b>COVID-19</b></p>
+            <p><b>Updated:</b> {new Date(date).toDateString()}</p>
+            <p>{about}<b>COVID-19</b></p>
             </div>
         </div>
 
