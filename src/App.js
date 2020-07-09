d0fa6cadeda345d2e0acb18f2152f0697178176a
kashifrazabb <kashifrazabb@gmail.com>
2020-07-09 00:31:48 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Result from './components/Result';
 import Select from './components/Select';
 import axios from 'axios';
 
 const App = () => {
-  const [countries,setCountries]=useState();
-  const [data,setData]=useState();
-  const [global,setGlobal]=useState();
-  const [date,setDate]=useState();
-  const [selectValue,setSelectValue]=useState('Global');
+  const [countries, setCountries] = useState();
+  const [data, setData] = useState();
+  const [global, setGlobal] = useState();
+  const [date, setDate] = useState();
+  const [selectValue, setSelectValue] = useState('Global');
 
-  const handleSelect=(e)=>setSelectValue(e.target.value)
+  const handleSelect = (e) => setSelectValue(e.target.value)
 
-  useEffect(()=>{
-    
-      (async ()=>{
-          const DATA=(await axios.get('https://api.covid19api.com/summary')).data;
-          setCountries(DATA.Countries.map(c=>c.Country))
-          setData(DATA.Countries)
-          setGlobal(DATA.Global)
-          setDate(DATA.Date)
-      })()
+  useEffect(() => {
 
-  },[])
+    (async () => {
+      const DATA = (await axios.get('https://api.covid19api.com/summary')).data;
+      setCountries(DATA.Countries.map(c => c.Country))
+      setData(DATA.Countries)
+      setGlobal(DATA.Global)
+      setDate(DATA.Date)
 
-  if (data){
-    
-      const INDEX=data.findIndex(i=>i.Country===selectValue);
-      if (selectValue!=='Global'){
-        var {TotalConfirmed,TotalRecovered,TotalDeaths}=data[INDEX];
-      }
-      else if (selectValue==='Global' && global){
-        var {TotalConfirmed,TotalRecovered,TotalDeaths}=global;
-      }
+    })()
+
+  }, [])
+
+
+  if (data) {
+
+    const INDEX = data.findIndex(i => i.Country === selectValue);
+    var TotalConfirmed, TotalRecovered, TotalDeaths, CountryCode;
+    if (selectValue !== 'Global') {
+      TotalConfirmed = data[INDEX].TotalConfirmed;
+      TotalRecovered = data[INDEX].TotalRecovered;
+      TotalDeaths = data[INDEX].TotalDeaths;
+      CountryCode = data[INDEX].CountryCode;
+    }
+    else if (selectValue === 'Global' && global) {
+      TotalConfirmed = global.TotalConfirmed;
+      TotalRecovered = global.TotalRecovered;
+      TotalDeaths = global.TotalDeaths;
+    }
 
   }
 
   return (
     <>
-        <Header/>
-        <Select countries={countries?countries:['Loading....']} handleSelect={handleSelect} 
-        selectValue={selectValue}/>
-        <Result TotalConfirmed={TotalConfirmed} TotalRecovered={TotalRecovered}
-        TotalDeaths={TotalDeaths} date={date}/>
+      <Header />
+      <Select countries={countries ? countries : ['Loading....']} handleSelect={handleSelect}
+        selectValue={selectValue} CountryCode={CountryCode} />
+      <Result TotalConfirmed={TotalConfirmed} TotalRecovered={TotalRecovered}
+        TotalDeaths={TotalDeaths} date={date} />
     </>
   );
 }
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const Select = ({countries,selectValue,handleSelect}) => {
+const Select = ({countries,CountryCode,selectValue,handleSelect}) => {
     return (
         <div className='container select-container'>
             <select onChange={handleSelect} value={selectValue} className='form-control shadow'>
-                <option value="Global">Global</option>
+                <option value="Global"> Global</option>
                 {countries.map(country=><option value={country}>{country}</option>)}
             </select>
+            {CountryCode?
+            <div className='text-center mt-5'>
+                <img src={`https://www.countryflags.io/${CountryCode}/flat/64.png`} alt='Flag'/>
+            </div>:null}
         </div>
     )
 }
